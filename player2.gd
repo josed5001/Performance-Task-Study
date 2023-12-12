a775959a57786b2e661d269399dbea7c72b4d495
josed5001 <122053779+josed5001@users.noreply.github.com>
2023-12-12 12:14:53 -0800

    Collision Setup

--- player2.gd
+++ player2.gd
@@ -1,11 +1,12 @@
-extends CharacterBody2D
+extends Area2D
+
 
 @onready var collision_shape_2d = $CollisionShape2D
 
 @export var speed = 400
 
 func _physics_process(delta):
-	velocity =  Vector2.ZERO
+	var velocity =  Vector2.ZERO
 	
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
@@ -20,3 +21,7 @@ func _physics_process(delta):
 		velocity = velocity.normalized() * speed
 	position += velocity * delta
 	
+
+
+func _on_body_entered(body):
+	hide()
